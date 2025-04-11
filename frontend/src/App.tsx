ee64ccccef5fab1e1d6d6c4ce4a6d4a92a8f0a80
Lorenz Kobe Managbanag <lorenzkobem@gmail.com>
2025-04-11 16:51:02 +0800

    feat: enhance search functionality with loading states and results display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,61 @@
 import axios from "axios";
-import { Loader, Search } from "lucide-react";
+import { Frown, Loader, Search } from "lucide-react";
 import { useState } from "react";
+import Results from "./components/Results.Component";
+import { ResultProps } from "./components/ResultContainer.Component";
+import { extractNextLinkHeader } from "./utils/common";
 
 function App() {
   const [search, setSearch] = useState("");
+  const [results, setResults] = useState<ResultProps[]>([]);
+  const [nextPage, setNextPage] = useState<string | null>("");
+
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingNextPage, setIsLoadingNextPage] = useState(false);
 
   const handleSearch = async () => {
     try {
       setIsLoading(true);
+      setResults([]);
+      setNextPage("");
+
       const response = await axios.post("http://localhost:8000/api/execute", {
         message: search,
       });
 
-      console.log(response.data);
+      const { data, next_page } = response.data;
+
+      setResults(data?.results || []);
+      setNextPage(extractNextLinkHeader(next_page));
     } catch (e) {
       console.error(e);
     } finally {
       setIsLoading(false);
+    }
+  };
+
+  const handlePageChange = async () => {
+    try {
+      setIsLoadingNextPage(true);
+      setNextPage("");
+
+      const response = await axios.post(
+        "http://localhost:8000/api/fetch-page",
+        {
+          link: nextPage,
+        }
+      );
+
+      const { data, next_page } = response.data;
+
+      if (data?.results.length > 0) {
+        setResults((prevResults) => [...prevResults, ...data.results]);
+        setNextPage(extractNextLinkHeader(next_page));
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoadingNextPage(false);
     }
   };
 
@@ -31,24 +69,69 @@ function App() {
           <p className="px-16 text-sm pt-2 text-center">
             A simple restaurant finder powered by bleeding edge AI tech
           </p>
-          <div className="w-full max-w-md mt-8 md:px-0 px-8">
+          <div className="w-full max-w-lg mt-8 md:px-0 px-8">
             <div className="flex flex-col">
               <textarea
                 placeholder="Search for restaurants..."
-                className="w-full px-6 py-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent resize-none"
+                className={`w-full px-6 py-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent resize-none ${
+                  isLoading && "bg-gray-300"
+                }`}
                 rows={6}
                 onChange={(e) => setSearch(e.target.value)}
+                disabled={isLoading}
               />
-                <button
+              <button
                 className={`flex items-center justify-center mt-4 py-3 rounded-md w-full text-sm ${
-                  !search ? 'bg-gray-300' : 'bg-black hover:cursor-pointer'
+                  !search || isLoading
+                    ? "bg-gray-300"
+                    : "bg-black hover:cursor-pointer"
                 } text-white`}
                 onClick={handleSearch}
-                disabled={!search}
-                >
-                {isLoading ? <><Loader>Sample</Loader></> : <><Search className="pe-2" /> Search</>}
-                
+                disabled={!search || isLoading}
+              >
+                {isLoading ? (
+                  <>
+                    <Loader size={16} className="me-2" /> Searching
+                  </>
+                ) : (
+                  <>
+                    <Search className="pe-2" /> Search
+                  </>
+                )}
               </button>
+            </div>
+
+            {!isLoading && (
+              <>
+                {results.length > 0 ? (
+                  <Results results={results} />
+                ) : (
+                  <div className="flex flex-row items-center justify-center mt-8">
+                    <Frown size={18} strokeWidth={3} className="me-2" />
+                    <p className="font-semibold">No Results</p>
+                  </div>
+                )}
+              </>
+            )}
+
+            <div className="flex flex-row items-center justify-center mt-8">
+              {isLoadingNextPage ? (
+                <>
+                  <Loader size={16} className="me-2" /> Loading
+                </>
+              ) : (
+                <>
+                  {nextPage && (
+                    <button
+                      onClick={handlePageChange}
+                      className="py-2 px-6 bg-black text-white rounded-full hover:cursor-pointer"
+                    >
+                      {" "}
+                      Load Next Page
+                    </button>
+                  )}
+                </>
+              )}
             </div>
           </div>
         </div>
