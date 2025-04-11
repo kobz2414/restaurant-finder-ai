01c71e55749400f2d5116eba6868e4b5f0aae2c3
Lorenz Kobe Managbanag <lorenzkobem@gmail.com>
2025-04-11 14:15:55 +0800

    feat: load environment variables

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 import os
 
 from openai import OpenAI
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
